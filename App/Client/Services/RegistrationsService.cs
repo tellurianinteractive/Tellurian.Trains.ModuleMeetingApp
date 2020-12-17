5bb8588bf147e5a8ecbc6911fc9604178c4d3dfb
Stefan Fjällemark <fjallemark@hotmail.com>
2020-12-17 19:49:42 +0100

    Refined fix of showning settings button on index page.

--- App/Client/Services/RegistrationsService.cs
+++ App/Client/Services/RegistrationsService.cs
@@ -1,7 +1,4 @@
 ﻿using Blazored.LocalStorage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Tellurian.Trains.MeetingApp.Contract;
